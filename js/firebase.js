0216f0f96d3f1103d20fb722be1158ca381ff72a
J3SS-drix <jullyamjessua08092005@gmail.com>
2026-06-19 19:45:46 -0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,26 +1,3 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-    getFirestore, 
-    collection, 
-    addDoc, 
-    updateDoc, 
-    deleteDoc, 
-    doc, 
-    onSnapshot 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBb6Y4aeYXLTu2wq2KtVS-JcBfulKqYnRc",
-  authDomain: "presente-ef9af.firebaseapp.com",
-  projectId: "presente-ef9af",
-  storageBucket: "presente-ef9af.firebasestorage.app",
-  messagingSenderId: "794715558769",
-  appId: "1:794715558769:web:e7341dc92f4a03795f1cd0",
-  measurementId: "G-PX0Q4RY8B8"
-};
-
 // Inicializar Firebase y el servicio de Firestore
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
